66b40d023757b6e6eda539321339842c6f643410
newta105 <119808370+newta105@users.noreply.github.com>
2023-09-11 15:36:29 +0300

    Create lab1_NoorAlresaini.php

---
+++ labs/lab1/lab1_NoorAlresaini.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+	echo "<h1> Noor Alresaini(202122364) </h1>";
+	echo $n = 1;
+	echo "<br>";
+	echo $s = "1";
+	echo "<br>";
+	echo $n1 = $n.$s;
+	echo "<br>";
+	echo $n2 = $n1. "10 little penguins";
+	echo "<br>";
+	echo $n3 = "hello";
+	echo "<br>";
+	echo $n4 = " world";
+	echo "<br>";
+	echo $n5 = $n3.$n4;
+	echo "<br>";
+	echo $n6 = $n3.$n4;
+	echo "<br>";
+?>
+</body>
+</html>
+
